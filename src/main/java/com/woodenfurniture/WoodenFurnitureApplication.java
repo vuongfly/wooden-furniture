c5db15a82eb9826e6b40ce40aa6b214da6516aed
vuongfly <vuongfly96@gmail.com>
2024-03-05 11:13:46 +0700

    init

---
+++ src/main/java/com/woodenfurniture/WoodenFurnitureApplication.java
@@ -0,0 +1,13 @@
+package com.woodenfurniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodenFurnitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodenFurnitureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woodenfurniture/WoodenFurnitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodenfurniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodenFurnitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
