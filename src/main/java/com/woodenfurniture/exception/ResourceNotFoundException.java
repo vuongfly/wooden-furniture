645b47a2cac3d253cebf33529cad166b6cd2e857
vuongfly <vuongfly96@gmail.com>
2025-04-09 12:28:47 +0700

    add custom error

---
+++ src/main/java/com/woodenfurniture/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.woodenfurniture.exception;
+
+import lombok.Data;
+
+@Data
+public class ResourceNotFoundException extends RuntimeException{
+    private String errorMessage;
+
+    public ResourceNotFoundException(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
