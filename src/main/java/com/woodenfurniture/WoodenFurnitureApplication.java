bf878b14efa9d96dd6e2201cfa76a86bb4a7b92c
vuongFly <vuongfly96@gmail.com>
2025-04-22 10:48:13 +0700

    update config export user

--- src/main/java/com/woodenfurniture/WoodenFurnitureApplication.java
+++ src/main/java/com/woodenfurniture/WoodenFurnitureApplication.java
@@ -2,10 +2,12 @@ package com.woodenfurniture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EntityScan("com.woodenfurniture")
 public class WoodenFurnitureApplication {
 
     public static void main(String[] args) {
